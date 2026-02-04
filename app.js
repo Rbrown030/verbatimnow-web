9b067bc7b98ce2c8d42f78c362b9ac0f71b91677
Riley Brown <44388697+Rbrown030@users.noreply.github.com>
2026-02-04 14:50:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 /* ========= CONFIG ========= */
 // PUT YOUR REAL CREATE-CHECKOUT ENDPOINT HERE
 // Example: https://abcd1234.lambda-url.us-east-1.on.aws/
-const CHECKOUT_ENDPOINT = "PASTE_YOUR_CREATE_CHECKOUT_URL_HERE";
+const CHECKOUT_ENDPOINT = https://3ydz2dudmxq7ksgd2nzvdp5hpq0xlypt.lambda-url.us-east-2.on.aws/;
 
 /* ========= PRICING ========= */
 const PRICE_PER_MIN = 0.50;
